e68210a586e42c53c7e3b376f0cc0a986641cee2
Dan Newman <dan.newman@mtbaldy.us>
2015-03-28 13:58:03 -0700

    Restore line which should not have been removed; removal causes framing to not appear when converting and sendind data directly out a serial port.

--- src/gpx/gpx.c
+++ src/gpx/gpx.c
@@ -5505,6 +5505,7 @@ int gpx_convert_and_send(Gpx *gpx, FILE *file_in, int sio_port,
         gpx_initialize(gpx, 0);
 
         gpx->flag.logMessages = 1;
+        gpx->flag.framingEnabled = 1;
         gpx->callbackHandler = (int (*)(Gpx*, void*, char*, size_t))port_handler;;
         gpx->callbackData = &sio;
     }
