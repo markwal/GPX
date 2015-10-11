77c6227e69bbbe05adc92032efdacb391b1d0d2d
Mark Walker <markwal@hotmail.com>
2015-10-11 15:32:16 -0700

    booleans and allow the map for Sailfish 7.8

--- src/shared/std_eeprommaps.h
+++ src/shared/std_eeprommaps.h
@@ -33,7 +33,7 @@
 #include "sailfish_7_7.h"
 
 EepromMap eepromMaps[] = {
-    { 707, 707, 0x80, eeprom_map_sailfish_7_7, (sizeof(eeprom_map_sailfish_7_7) / sizeof(EepromMapping)) },
+    { 707, 708, 0x80, eeprom_map_sailfish_7_7, (sizeof(eeprom_map_sailfish_7_7) / sizeof(EepromMapping)) },
 };
 
 #define eepromMapCount (sizeof(eepromMaps) / sizeof(EepromMap))
